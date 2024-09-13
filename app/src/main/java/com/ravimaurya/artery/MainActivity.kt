39455ab16328783084b0e1a8d7564d5876758f84
Ravi Maurya <riderrobo08@gmail.com>
2024-09-13 19:03:31 +0530

    Create Project and Initialize Git

---
+++ app/src/main/java/com/ravimaurya/artery/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.ravimaurya.artery
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ravimaurya.artery.ui.theme.ArteryTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ArteryTheme {
+
+
+            }
+        }
+    }
+}
+
