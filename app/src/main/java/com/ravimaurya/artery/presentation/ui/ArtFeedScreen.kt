c04198c6b9d4476f90f33e13ec4c2d49f23497f6
Ravi Maurya <ravithemauryas@gmail.com>
2024-09-18 23:09:50 +0530

    Design Ui for Screens

--- app/src/main/java/com/ravimaurya/artery/presentation/ui/ArtFeedScreen.kt
+++ app/src/main/java/com/ravimaurya/artery/presentation/ui/ArtFeedScreen.kt
@@ -1,22 +1,117 @@
 package com.ravimaurya.artery.presentation.ui
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ravimaurya.artery.R
 
+@OptIn(ExperimentalMaterial3Api::class)
+//@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun ArtFeedScreen(modifier: Modifier = Modifier) {
+    
 
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Feed")
+        Column(
+            modifier = Modifier.fillMaxSize().padding(),
+        ) {
+
+            ArtFeedItem()
+        }
+
+
+
+
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ArtFeedItem(){
+    Box(
+        modifier = Modifier.fillMaxSize().background(Color.Black),
+        contentAlignment = Alignment.BottomStart
+    ){
+        Image(
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Fit,
+            painter = painterResource(R.drawable.test),
+            contentDescription = "")
+        Column(
+            Modifier.fillMaxWidth().padding(5.dp),
+            horizontalAlignment = Alignment.Start
+        ) {
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+        ){
+            Icon(
+                tint = Color.White,
+                modifier = Modifier
+                    .size(40.dp)
+                    .border(1.dp, Color.Cyan)
+                    .clip(RoundedCornerShape(50f))
+                    .background(Color.Blue),
+                painter = painterResource(R.drawable.ic_launcher_foreground),
+                contentDescription = "")
+            Column(
+                modifier = Modifier
+                    .padding(start = 5.dp)
+            ) {
+                Text("User Name", fontSize = 10.sp, color = Color.White)
+            }
+            Text(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(4.dp))
+                    .border(1.dp, color = Color.Cyan)
+                    .background(Color.Cyan.copy(alpha = 0.5f))
+                    .padding(2.dp),
+                text = "Follow",
+                fontSize = 10.sp,
+                color = Color.White)
+
+        }
+            Text(
+                text = "This is my art project " +
+                    "where user can share their" +
+                    " talent to world and also can " +
+                    "explore others talent and can" +
+                    " follow to each other, arts such" +
+                    " as digital painting, illustration, " +
+                    "3d model, graphic design etc.",
+                softWrap = true,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                color = Color.White)
+        }
+
     }
-
 }
--- app/src/main/java/com/ravimaurya/artery/presentation/ui/HomeScreen.kt
+++ app/src/main/java/com/ravimaurya/artery/presentation/ui/HomeScreen.kt
@@ -1,12 +1,41 @@
 package com.ravimaurya.artery.presentation.ui
 
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.ThumbUp
+import androidx.compose.material.icons.outlined.Bookmark
+import androidx.compose.material.icons.outlined.ThumbUp
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ravimaurya.artery.R
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
@@ -15,6 +44,81 @@ fun HomeScreen(modifier: Modifier = Modifier) {
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Home")
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ArtItem(){
+
+    Column (
+        modifier = Modifier.fillMaxWidth().padding(8.dp)
+    ){
+        Box(
+            modifier = Modifier.fillMaxWidth().fillMaxHeight(.5f),
+            contentAlignment = Alignment.TopStart
+        ) {
+
+            Image(
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.fillMaxSize().clip(RoundedCornerShape(16.dp)),
+                painter = painterResource(R.drawable.test), contentDescription =  "")
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+
+                    Image(
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(RoundedCornerShape(50)),
+                        painter = painterResource(R.drawable.ic_launcher_foreground),
+                        contentDescription = "")
+                    Column (verticalArrangement = Arrangement.Center){
+
+                        Text(color = Color.White, text = "User Name", fontSize = 16.sp, modifier = Modifier.padding(start = 8.dp))
+                        Text(color = Color.LightGray, text = "emaiaill", fontSize = 16.sp, modifier = Modifier.padding(start = 8.dp))
+                    }
+                }
+                Icon(Icons.Filled.MoreVert, contentDescription = "", tint = Color.White)
+            }
+        }
+        ArtComplements()
+        Text(text = "This is art description...", Modifier.padding(vertical = 5.dp))
+    }
+
+
+
+}
+
+//@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ArtComplements(){
+    Row (
+        modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp),
+        horizontalArrangement = Arrangement.SpaceAround
+    ){
+        IconButton(onClick = {}) {
+            Icon(Icons.Outlined.ThumbUp, "like")
+        }
+        IconButton(onClick = {}) {
+            Icon(Icons.Filled.FavoriteBorder, "favorite")
+        }
+        IconButton(onClick = {}) {
+            Icon(Icons.Outlined.Bookmark, "bookmark")
+        }
+        IconButton(onClick = {}) {
+            Icon(Icons.Filled.Share, "share")
+        }
+
     }
 }
